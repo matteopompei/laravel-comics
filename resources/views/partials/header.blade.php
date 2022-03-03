2c6d98a648fb402385c383d250b9e9c735431ec2
Matteo Pompei <datxizzan@gmail.com>
2022-03-03 19:53:00 +0100

    Header fatto

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,19 @@
+<header>
+  <div class="container">
+    <a href="#"><img src="{{ asset('img/dc-logo.png') }}" alt="DC" /></a>
+    <nav>
+      <ul>
+        <li><a href="#">Characters</a></li>
+        <li><a href="#">Comics</a></li>
+        <li><a href="#">Movies</a></li>
+        <li><a href="#">TV</a></li>
+        <li><a href="#">Games</a></li>
+        <li><a href="#">Collectibles</a></li>
+        <li><a href="#">Videos</a></li>
+        <li><a href="#">Fans</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Shop</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
