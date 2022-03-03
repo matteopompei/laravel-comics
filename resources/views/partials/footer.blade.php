3af614fff8d12e3ace90f670713d7add503d798b
Matteo Pompei <datxizzan@gmail.com>
2022-03-03 20:10:26 +0100

    Footer creato

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,159 @@
+<footer>
+  <section class="top">
+    <div class="container">
+      <nav>
+        <ul>
+          <li>
+            <a href="#">
+              <img src="{{ asset('img/buy-comics-digital-comics.png') }}" alt="Digital Comics" />
+              <span>Digital Comics</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="{{ asset('img/buy-comics-merchandise.png') }}" alt="DC Merchandise" />
+              <span>DC Merchandise</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="{{ asset('img/buy-comics-subscriptions.png') }}" alt="Subscrition" />
+              <span>Subscrition</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="{{ asset('img/buy-comics-shop-locator.png') }}" alt="Comic Shop Locator" />
+              <span>Comic Shop Locator</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="{{ asset('img/buy-dc-power-visa.svg') }}" alt="DC Power Visa" />
+              <span>DC Power Visa</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </section>
+  <section class="mid">
+    <div class="container">
+      <nav>
+        <div>
+          <h3>DC Comics</h3>
+          <ul>
+            <li>
+              <a href="#">Characters</a>
+            </li>
+            <li>
+              <a href="#">Comics</a>
+            </li>
+            <li>
+              <a href="#">Movies</a>
+            </li>
+            <li>
+              <a href="#">TV</a>
+            </li>
+            <li>
+              <a href="#">Games</a>
+            </li>
+            <li>
+              <a href="#">Videos</a>
+            </li>
+            <li>
+              <a href="#">News</a>
+            </li>
+          </ul>
+
+          <h3>Shop</h3>
+          <ul>
+            <li>
+              <a href="#">Shop DC</a>
+            </li>
+            <li>
+              <a href="#">Shop DC Collectibles</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3>DC</h3>
+          <ul>
+            <li>
+              <a href="#">Terms of use</a>
+            </li>
+            <li>
+              <a href="#">Privacy policy (new)</a>
+            </li>
+            <li>
+              <a href="#">Ad choises</a>
+            </li>
+            <li>
+              <a href="#">Advertising</a>
+            </li>
+            <li>
+              <a href="#">Jobs</a>
+            </li>
+            <li>
+              <a href="#">Subscriptions</a>
+            </li>
+            <li>
+              <a href="#">Talent Workshops</a>
+            </li>
+            <li>
+              <a href="#">CPSC Certificates</a>
+            </li>
+            <li>
+              <a href="#">Ratings</a>
+            </li>
+            <li>
+              <a href="#">Shop Help</a>
+            </li>
+            <li>
+              <a href="#">Contacts us</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3>Sites</h3>
+          <ul>
+            <li><a href="#">DC</a></li>
+            <li><a href="#">MAD Magazione</a></li>
+            <li><a href="#">DS Kids</a></li>
+            <li><a href="#">DS Universe</a></li>
+            <li><a href="#">DS Power Visa</a></li>
+          </ul>
+        </div>
+      </nav>
+
+      <img src="{{ asset('img/dc-logo-bg.png') }}" alt="" />
+    </div>
+  </section>
+  <section class="bot">
+    <div class="container">
+      <a href="#" class="signup">Sign-up now!</a>
+
+      <div class="social">
+        <h3>Follow us</h3>
+
+        <ul>
+          <li>
+            <a href="#"><img src="{{ asset('img/footer-facebook.png') }}" alt="Facebook" /></a>
+          </li>
+          <li>
+            <a href="#"><img src="{{ asset('img/footer-twitter.png') }}" alt="Twitter" /></a>
+          </li>
+          <li>
+            <a href="#"><img src="{{ asset('img/footer-youtube.png') }}" alt="YouTube" /></a>
+          </li>
+          <li>
+            <a href="#"><img src="{{ asset('img/footer-pinterest.png') }}" alt="Pinterest" /></a>
+          </li>
+          <li>
+            <a href="#"><img src="{{ asset('img/footer-periscope.png') }}" alt="Periscope" /></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</footer>
